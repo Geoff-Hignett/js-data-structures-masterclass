fa609df367fb5612040e2df5a88e927be5541a98
Geoff-Hignett <geoffhig@hotmail.com>
2019-06-25 18:55:48 +0100

    graph traversal added

---
+++ graph-class.js
@@ -0,0 +1,56 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1);
+  }
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const toRemove = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, toRemove);
+    }
+    delete this.adjacencyList[vertex];
+  }
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbour => {
+        if (!visited[neighbour]) {
+          return dfs(neighbour);
+        }
+      });
+    })(start);
+    return result;
+  }
+}
+
+let graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
+
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'E');
+graph.addEdge('D', 'E');
+graph.addEdge('D', 'F');
+graph.addEdge('E', 'F');
